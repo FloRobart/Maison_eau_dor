f54bd2b3fa5240c82bdb15715e55ed20fb87d86f
Alexandre-Crespin <eragon7237@gmail.com>
2023-12-14 01:35:12 +0100

    Amélio Header + carrousel fonctionnel pour 1440 et +

--- public/assets/js/accueil.js
+++ public/assets/js/accueil.js
@@ -1,26 +1,53 @@
 let currentIndex = 0;
 const slidesContainer = document.querySelector('.carousel-accueil');
-const totalSlides = 4;
+const totalSlides = 7; // Modifiez le total des slides à 6
 const slidesToShow = 3;
-const slideWidth = 480; // Largeur fixe de chaque slide en pixels
+let slideWidth;
+
+function calculateSlideWidth() {
+    slideWidth = slidesContainer.offsetWidth / slidesToShow;
+}
 
 function showSlide(index) {
     const offset = -index * slideWidth + 'px';
     slidesContainer.style.transform = 'translateX(' + offset + ')';
 }
 
-function prevSlide() {
-    currentIndex = (currentIndex - 1 + totalSlides) % totalSlides;
-    showSlide(currentIndex);
-}
-
 function nextSlide() {
     currentIndex = (currentIndex + 1) % totalSlides;
+
+    // Si l'index atteint (totalSlides - slidesToShow), réinitialisez l'index à zéro
+    if (currentIndex === (totalSlides - slidesToShow)) {
+        currentIndex = 0;
+    }
+
     showSlide(currentIndex);
 }
 
+function prevSlide() {
+    currentIndex = (currentIndex - 1 + 4) % 4;
+
+    // Si l'index atteint la première position, réinitialisez l'index à (totalSlides - slidesToShow)
+    if (currentIndex === (totalSlides - slidesToShow)) {
+        currentIndex = totalSlides - slidesToShow;
+    }
+
+    showSlide(currentIndex);
+}
+
+
+
 document.addEventListener('DOMContentLoaded', function () {
+    calculateSlideWidth();
+    
+    // Recalculer la largeur des slides lors du redimensionnement de la fenêtre
+    window.addEventListener('resize', function () {
+        calculateSlideWidth();
+        showSlide(currentIndex);
+    });
+
     slidesContainer.style.width = totalSlides * slideWidth + 'px';
+    
     document.querySelectorAll('.slide-accueil').forEach(function (slide) {
         slide.style.width = slideWidth + 'px';
     });
